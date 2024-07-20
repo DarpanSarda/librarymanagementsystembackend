7244df69f37f02dbec7f8e3b85f407d9e2a0dcad
DarpanSarda <darpansarda7@gmail.com>
2024-07-20 10:40:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const {DbConnect} = require('./Config/Dbconnect');
+
+// const UserRoute = require("./Routes/User/UserRoute");
+// const BookRoute = require("./Routes/Books/BookRoute");
+// const IssueRoute = require("./Routes/Issue/IssueRoute");
+// const TrendRoute = require("./Routes/Trending/TrendingRoute")
+
+const app = express();
+
+const PORT = 3001;
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/",(req,res)=>{res.send("hello")})
+
+// app.use("/api/v1/auth",UserRoute);
+// app.use("/api/v1/books",BookRoute);
+// app.use("/api/v1/issue",IssueRoute);
+// app.use("/api/v1/trend",TrendRoute);
+
+app.listen(PORT,async()=>{
+    await DbConnect();
+    console.log("server started")
+})
